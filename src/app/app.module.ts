0bf08b506aea765df221f61f8f87f96d4d4cd6ff
cojic <102799668+cojic@users.noreply.github.com>
2024-12-26 22:06:39 +0100

    feat: Create accommodation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './features/user/user.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms'; // For [(ngModel)] if needed
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AccommodationsListComponent } from './features/accommodation/accommodations-list/accommodations-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccommodationsListComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +24,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CoreModule,
     UserModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
