4ba619846e020f8b716ed769017817c765f5817f
Sara Mihajlović <33412004+Mihajlovicka@users.noreply.github.com>
2025-01-26 14:15:17 +0100

    feat: cicd pipeline

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  auth_api: '/auth/api/',
+  accommodation_api: '/accommodation/api/',
+  booking_api: '/booking/api/',
+  notification_api: '/notification/api/',
+};
